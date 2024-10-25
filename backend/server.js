6512e109ebaf902a4f94bb262d7522f683aceb6d
vortix2950 <vortix2950@ksu.edu>
2024-10-25 12:08:15 +0300

    fixed admin/manager auth

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ async function startConnection() {
 let port = process.env.BACKEND_PORT 
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'], // Include PATCH here
+  }));
+
 app.use(router)
 
 
